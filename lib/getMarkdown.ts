bba2c6ebaf0a8c1ef5ba24d7c40d859d76d0c7fd
theodoreche <2226390@gmail.com>
2021-11-22 18:48:27 +0100

    fix build

--- lib/getMarkdown.ts
+++ lib/getMarkdown.ts
@@ -1,9 +1,10 @@
-import fs from 'fs'
-import { join } from 'path'
+import { readFileSync } from 'fs'
+import { resolve, join } from 'path'
 import matter from 'gray-matter'
+import paths from '../paths'
 
 export function getMarkdown(path: string): matter.GrayMatterFile<string> {
-  const fullPath = join(process.cwd(), path)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const fullPath = join(paths.root, path)
+  const fileContents = readFileSync(fullPath, 'utf8')
   return matter(fileContents)
 }
